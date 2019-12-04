b9f92fb5791c3818fe19c2d73bd82fc9fbc31ed6
ollinieminen <otn987@gmail.com>
2019-12-03 22:24:08 -0800

    feat(MainPage): create component

---
+++ src/components/mainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class MainPage extends React.Component {
+
+  render() {
+
+    return (
+      <main>
+        <h1>Welcome to Pi Grill!</h1>
+      </main>
+    )
+  }
+}

d8319676dd557e180fa19096af9b7eb2374bef21
ollinieminen <otn987@gmail.com>
2019-12-08 23:55:57 -0800

    feat(App): add context

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import MainPage from './components/main-page/MainPage'
 
+
 import './App.css';
+import SettingsContextProvider from './contexts/settingsContext';
 
-const App: React.FC = () => {
+// const App: React.FC = () => {
+//   return (
+
+//     <div className="App">
+//       <MainPage />
+//     </div>
+//   );
+// }
+
+function App() {
   return (
+    <div>
+      <SettingsContextProvider>
+        <MainPage />
 
-    <div className="App">
-      <MainPage />
+      </SettingsContextProvider>
     </div>
-  );
+  )
+
 }
 
 export default App;

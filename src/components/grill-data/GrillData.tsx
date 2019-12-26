b24731e85cdf89fc97a89b8d5a66fa061e9df261
ollinieminen <otn987@gmail.com>
2019-12-26 06:39:30 -0800

    feat: update grill data

--- src/components/grill-data/GrillData.tsx
+++ src/components/grill-data/GrillData.tsx
@@ -4,22 +4,34 @@ import { SettingsContext } from '../../contexts/settingsContext';
 export default function GrillData() {
 
   const grillControls = useContext(SettingsContext)
-  const { grillParams } = grillControls
+  const { grillParams, setGrillParams } = grillControls
 
   const grillStatus = grillParams.grillOn ? 'Grill On' : 'Grill Off';
   const grillMode = grillParams.smokeOn ? 'Smoke' : 'Grill'
 
 
   return (
-    <ul>
-      <li> {grillStatus} / {grillMode} </li>
-      <li> Grill Temperature: / Target {grillParams.targetGrillTemp}</li>
-      <div className={`display_${grillParams.probeOneOn.toString()}`}>
-        <li>{grillParams.probeOneName} Target Temp: {grillParams.targetProbeOneTemp}</li>
-      </div>
-      <div className={`display_${grillParams.probeTwoOn.toString()}`}>
-        <li>{grillParams.probeTwoName} Target Temp: {grillParams.targetProbeTwoTemp}</li>
-      </div>
-    </ul>
+    <>
+      <ul>
+        <li> Grill Status: {grillStatus}</li>
+        <li> Grill Mode: {grillMode}</li>
+        <li> Target Grill Temp: {grillParams.targetGrillTemp}</li>
+        <li> Grill Temp: N/A</li>
+        <div className={`display_${grillParams.probeOneOn.toString()}`}>
+          <li>{grillParams.probeOneName} Target Temp: {grillParams.targetProbeOneTemp}</li>
+          <li>{grillParams.probeOneName} Temp : N/A</li>
+        </div>
+        <div className={`display_${grillParams.probeTwoOn.toString()}`}>
+          <li>{grillParams.probeTwoName} Target Temp: {grillParams.targetProbeTwoTemp}</li>
+          <li>{grillParams.probeTwoName} Temp: N/A</li>
+        </div>
+      </ul>
+      < button onClick={() => setGrillParams({ ...grillParams, grillOn: !grillParams.grillOn })}>
+        {grillControls.grillParams.grillOn === true ? 'Grill Off' : 'Grill On'}
+      </button>
+      <button onClick={() => setGrillParams({ ...grillParams, smokeOn: !grillParams.smokeOn })}>
+        {grillParams.smokeOn === true ? 'Smoke Mode' : 'Grill Mode'}
+      </button>
+    </>
   )
 } 

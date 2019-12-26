2a4089301b9b6e819059ed0482eae51a9df80cad
ollinieminen <otn987@gmail.com>
2019-12-26 01:56:57 -0800

    feat: create GrillData component

--- src/components/grill-data/GrillData.tsx
+++ src/components/grill-data/GrillData.tsx
@@ -1,12 +1,25 @@
 import React, { useContext } from 'react';
+import { SettingsContext } from '../../contexts/settingsContext';
 
 export default function GrillData() {
 
+  const grillControls = useContext(SettingsContext)
+  const { grillParams } = grillControls
+
+  const grillStatus = grillParams.grillOn ? 'Grill On' : 'Grill Off';
+  const grillMode = grillParams.smokeOn ? 'Smoke' : 'Grill'
+
+
   return (
     <ul>
-      <li> Grill Status: </li>
-      <li> Grill Mode: </li>
+      <li> {grillStatus} / {grillMode} </li>
       <li> Grill Temperature:</li>
+      <div className={`display_${grillParams.probeOneOn.toString()}`}>
+        <li>{grillParams.probeOneName} Target Temp: {grillParams.targetProbeOneTemp}</li>
+      </div>
+      <div className={`display_${grillParams.probeTwoOn.toString()}`}>
+        <li>{grillParams.probeTwoName} Target Temp: {grillParams.targetProbeTwoTemp}</li>
+      </div>
     </ul>
   )
-}
+} 

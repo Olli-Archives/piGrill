e272d4f391b7b676099a245d76b0e0b21229f354
ollinieminen <otn987@gmail.com>
2020-01-12 09:18:54 -0800

    feat: can toggle grill on/off from UI

--- src/components/grill-data/GrillData.tsx
+++ src/components/grill-data/GrillData.tsx
@@ -1,8 +1,13 @@
 import React, { useContext } from 'react';
 import { SettingsContext } from '../../contexts/settingsContext';
 import axios, { AxiosPromise, AxiosRequestConfig, Method } from 'axios';
+import { AxiosService } from '../../service/axios';
+
+
 
 export default function GrillData() {
+
+  const axiosService = new AxiosService('http://192.168.1.16:3001/')
 
   const grillControls = useContext(SettingsContext)
   const { grillParams, setGrillParams } = grillControls
@@ -30,10 +35,20 @@ export default function GrillData() {
           <li>{grillParams.probeTwoName} Temp: N/A</li>
         </div>
       </ul>
-      < button onClick={() => setGrillParams({ ...grillParams, grillOn: !grillParams.grillOn })}>
+      < button onClick={() => {
+        setGrillParams({ ...grillParams, grillOn: !grillParams.grillOn })
+        axiosService.post({
+          endPoint: 'grill-state',
+          body: {
+            state: `${grillParams.grillOn ? 'off' : 'on'}`
+          }
+        })
+      }}>
         {grillControls.grillParams.grillOn === true ? 'Grill Off' : 'Grill On'}
       </button>
-      <button onClick={() => setGrillParams({ ...grillParams, smokeOn: !grillParams.smokeOn })}>
+      <button onClick={
+        () => setGrillParams({ ...grillParams, smokeOn: !grillParams.smokeOn })
+      }>
         {grillParams.smokeOn === true ? 'Smoke Mode' : 'Grill Mode'}
       </button>
       <button onClick={() => { api() }}>SUBMIT CHANGES</button>
--- src/service/axios.tsx
+++ src/service/axios.tsx
@@ -1,3 +1,29 @@
+import React, { Component } from 'react';
 import axios, { AxiosPromise, AxiosRequestConfig, Method } from 'axios';
+
+export class AxiosService {
+  url: string;
+
+  constructor(url: string) {
+    this.url = url
+  }
+
+  post(options: Options) {
+    console.log('posting')
+    axios.post(`${this.url}${options.endPoint}`, options.body)
+      .then(res => { console.log(res) })
+      .catch(e => console.log(e))
+
+  }
+}
+
+interface Options {
+  body: {},
+  endPoint: string
+}
+
+
+
+
 
 
